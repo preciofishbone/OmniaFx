f6a28c15853c858e09808e1a7a0bf94dd9f9c615
Martin Löfberg <martin.lofberg@fishbonesystems.com>
2019-10-07 10:09:21 +0200

    Added custom font and styling overirdes via css/styleflow

---
+++ src/Examples/Ux/CustomFonts/Omnia.Fx.Examples.Ux.CustomFonts/client/styles/styleoverides.manifest.ts
@@ -0,0 +1,11 @@
+﻿import { Composer } from '@omnia/tooling/composers';
+import { Guid, OmniaResourceManifests, OmniaService } from '@omnia/fx-models';
+
+
+Composer.registerManifest("6132A71A-4154-4A54-82A1-CB213AA3EEDD", "CustomFonts.Core")
+    .registerResources({ resourcePaths: ["./omniaoverrides.min.css", "./styleoverrides.js"] })
+    .withExternalDependency(OmniaService.Id, OmniaResourceManifests.FxUx).done()
+    .withLoadRules().loadIfManifestLoaded({
+        omniaServiceId: OmniaService.Id.toString(),
+        resourceId: OmniaResourceManifests.FxUx.toString()
+    });
