dd498b5373cd2018d14900f3fb7d3d5c8d275360
Andreas Ekdahl <andy@ekdahls.net>
2021-06-18 14:46:41 +0200

    Small updates

--- src/Examples/Projects/WebAppWithDb/Omnia.Fx.Examples.WebAppWithDb.Web/omnia.service.ts
+++ src/Examples/Projects/WebAppWithDb/Omnia.Fx.Examples.WebAppWithDb.Web/omnia.service.ts
@@ -1,8 +1,9 @@
 import { Composer, DevelopmentEnvironment } from "@omnia/tooling/composers";
 import { Guid } from '@omnia/fx/models';
+import { WebAppWithDbService } from "./client/models/ManifestIds";
 
 Composer
-    .registerManifest(new Guid("aede937e-76bf-4a48-a156-623d0a116564"), "Omnia.Fx.Examples.WebAppWithDb.Web")
+    .registerManifest(WebAppWithDbService.Id, "Omnia.Fx.Examples.WebAppWithDb.Web")
     .registerService({ description: "Description of Omnia.Fx.Examples.WebAppWithDb.Web" })
     .AsWebApp()
     .withBuildOptions({
@@ -16,6 +17,10 @@ Composer
                 minChunks: 2
             }
         }
+    })
+    .requestSqlDatabase({
+        //Specify your own unique id, later used as reference to the DB generated for you.
+        uniqueId: new Guid("f8debb44-be08-4ae2-9cf5-c1cebc839123")
     });
     
    
